acf284d9416822e08317a77dcf6b9eeb46d93c18
Виктор Краснов <vickr@inbox.ru>
2021-09-15 07:25:53 +0300

    mongoose: added collection creation and insertion of documents

--- mongoose/app.js
+++ mongoose/app.js
@@ -8,3 +8,50 @@ const dbName = "fruitsDB";
 
 // Connect to the database (it is created if necessary)
 mongoose.connect(url + dbName, { useNewUrlParser: true });
+
+// Create schema of a collection element
+const fruitSchema = new mongoose.Schema({
+    name: String,
+    rating: Number,
+    review: String
+});
+
+// Create a collection "fruits"
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+// Create a single document
+const fruit = new Fruit({
+    name: "Apple",
+    rating: 7,
+    review: "Pretty solid as a fruit."
+});
+
+// Save it to the collection
+fruit.save();
+
+// Create and same several documents
+const kiwi = new Fruit({
+    name: "Kiwi",
+    rating: 10,
+    review: "The best fruit!"
+});
+
+const orange = new Fruit({
+    name: "Orange",
+    rating: 4,
+    review: "Too sour for me"
+});
+
+const banana = new Fruit({
+    name: "Banana",
+    rating: 3,
+    review: "Weird texture"
+});
+
+Fruit.insertMany([kiwi, orange, banana], function(err) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("Successfully saved all the fruits to the database");
+    }
+});
