a4419d958790b05eab05947f52096b726012e3ce
Виктор Краснов <vickr@inbox.ru>
2021-09-18 12:17:57 +0300

    todolist: prepare to deployment on heroku

--- todolist/app.js
+++ todolist/app.js
@@ -144,6 +144,7 @@ app.get("/about", function(req, res) {
   res.render("about");
 });
 
+let port = process.env.PORT || 3000;
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+  console.log("Server started on port " + port);
 });
