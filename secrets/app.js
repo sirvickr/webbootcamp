33fbf0c8da9b4c2c6832263d4cf4b656e4e2fd4e
Виктор Краснов <vickr@inbox.ru>
2021-09-21 09:55:38 +0300

    secrets: Level 1 - Register users with name and password

--- secrets/app.js
+++ secrets/app.js
@@ -3,12 +3,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs"); // interestingly, everything works without it
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = mongoose.model("User", userSchema);
 
 app.get("/", function(req, res) {
     res.render("home");
@@ -19,9 +29,38 @@ app.get("/login", function(req, res) {
     res.render("login");
 });
 
+app.post("/login", function(req, res) {
+    User.findOne({
+        email: req.body.username,
+        password: req.body.password
+    }, function(err, user) {
+        if(err) {
+            console.log("Authentication error:", err);
+        } else if(!user) {
+            res.send("Wrong email or password");
+        } else {
+            res.render("secrets");
+        }
+    });
+});
+
 
 app.get("/register", function(req, res) {
     res.render("register");
+});
+
+app.post("/register", function(req, res) {
+    const user = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    user.save(function(err) {
+        if(!err) {
+            res.render("secrets");
+        } else {
+            console.log("Registration error:", err);
+        }
+    });
 });
 
 
